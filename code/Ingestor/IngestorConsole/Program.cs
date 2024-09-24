9010e07bd102523c30fce9c198a41d9fbad220fb
vplauzon <vincent_lauzon@yahoo.com.au>
2024-09-24 19:14:30 -0400

    .

---
+++ code/Ingestor/IngestorConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+
+namespace IngestorConsole
+{
+    internal class Program
+    {
+        public static string AssemblyVersion
+        {
+            get
+            {
+                var version = typeof(Program).Assembly.GetName().Version;
+                var versionText = version == null
+                    ? "<VERSION MISSING>"
+                    : version.ToString();
+
+                return versionText;
+            }
+        }
+
+        internal static async Task<int> Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+
+            Console.WriteLine();
+            Console.WriteLine($"Kusto Ingestor Console ");
+            Console.WriteLine();
+
+            await Task.CompletedTask;
+
+            return 0;
+        }
+    }
+}
