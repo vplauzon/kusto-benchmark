8f51625df115d58e36d158d267cba45a76c869a5
vplauzon <vincent_lauzon@yahoo.com.au>
2024-09-24 20:55:47 -0400

    .

--- code/Ingestor/IngestorConsole/MainOrchestration.cs
+++ code/Ingestor/IngestorConsole/MainOrchestration.cs
@@ -1,20 +1,63 @@
 ﻿
+using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+
 namespace IngestorConsole
 {
     internal class MainOrchestration : IAsyncDisposable
     {
+        private readonly string _sampleText;
+
         #region Constructors
-        private MainOrchestration()
+        private MainOrchestration(string sampleText)
         {
+            _sampleText = sampleText;
         }
 
         public static async Task<MainOrchestration> CreateAsync(
             CommandLineOptions options,
             CancellationToken token)
         {
-            await Task.CompletedTask;
+            var credentials = CreateCredentials(options.Authentication);
+            var blobClient = CreateBlobClient(options.Source, credentials);
+            var sampleText = await LoadBlobAsync(blobClient);
 
-            return new MainOrchestration();
+            return new MainOrchestration(sampleText);
+        }
+
+        private static TokenCredential CreateCredentials(string authentication)
+        {
+            if (string.IsNullOrWhiteSpace(authentication))
+            {
+                return new DefaultAzureCredential();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private static BlobClient CreateBlobClient(string source, TokenCredential credentials)
+        {
+            var sourceUri = new Uri(source);
+            var segments = sourceUri.Segments;
+            var containerUri = new Uri($"{sourceUri.Scheme}://{sourceUri.Host}/{segments[1]}");
+            var blobName = string.Join("/", segments.Skip(2));
+            var containerClient = new BlobContainerClient(containerUri, credentials);
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            return blobClient;
+        }
+
+        private static async Task<string> LoadBlobAsync(BlobClient blobClient)
+        {
+            var download = await blobClient.DownloadAsync();
+
+            using (var reader = new StreamReader(download.Value.Content))
+            {
+                return await reader.ReadToEndAsync();
+            }
         }
         #endregion
 
