44ee27527ec1f345e77bbf5914dc307679f06fd6
vplauzon <vincent_lauzon@yahoo.com.au>
2024-09-27 17:25:29 -0400

    .

--- code/IngestorConsole/EventGenerator.cs
+++ code/IngestorConsole/EventGenerator.cs
@@ -1,15 +1,22 @@
-﻿using System.Text;
+﻿using System.Collections.Immutable;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace IngestorConsole
 {
     internal class EventGenerator
     {
-        private readonly string _template;
+        #region Inner types
+        private record TemplateGenerator(int Index, int Length, Func<string> Generator);
+        #endregion
+
+        private readonly IImmutableList<Func<string>> _generators;
 
         #region Constructors
-        private EventGenerator(string template)
+        private EventGenerator(IEnumerable<Func<string>> generators)
         {
-            _template = template;
+            _generators = generators.ToImmutableArray();
         }
 
         internal static async Task<EventGenerator> CreateAsync(
@@ -19,13 +26,66 @@ namespace IngestorConsole
         {
             await Task.CompletedTask;
 
-            return new EventGenerator(template);
+            var generators = StringGenerators(template);
+
+            return new EventGenerator(generators);
+        }
+
+        private static IEnumerable<TemplateGenerator> ExtractTimestampNow(string template)
+        {
+            const string PATTERN = "TimestampNow()";
+
+            var index = template.IndexOf(PATTERN);
+
+            while (index != -1)
+            {
+                yield return new TemplateGenerator(
+                    index,
+                    PATTERN.Length,
+                    () => DateTime.UtcNow.ToString());
+                template = template.Substring(index + PATTERN.Length);
+                index = template.IndexOf(PATTERN);
+            }
+        }
+
+        private static IEnumerable<Func<string>> StringGenerators(string template)
+        {
+            var timestampNowGenerators = ExtractTimestampNow(template);
+            var generators = timestampNowGenerators
+                .OrderBy(g => g.Index)
+                .ToImmutableArray();
+            var index = 0;
+
+            foreach (var generator in generators)
+            {
+                if (generator.Index != index)
+                {
+                    var text = template.Substring(index, generator.Index - index);
+
+                    yield return () => text;
+                }
+                yield return generator.Generator;
+                index = generator.Index + generator.Length;
+            }
+            if (index != template.Length)
+            {
+                var text = template.Substring(index);
+
+                yield return () => text;
+            }
         }
         #endregion
 
         public void GenerateEvent(TextWriter writer)
         {
-            writer.WriteLine(_template);
+#if DEBUG
+            var text = string.Concat(_generators.Select(g => g()));
+#endif
+            foreach(var generator in _generators)
+            {
+                writer.Write(generator());
+            }
+            writer.WriteLine();
         }
     }
 }
