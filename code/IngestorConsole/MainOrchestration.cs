e3e02e24c8749ca2e91893bc544c13e3d88a5963
vplauzon <vincent_lauzon@yahoo.com.au>
2024-09-26 21:14:42 -0400

    .

--- code/IngestorConsole/MainOrchestration.cs
+++ code/IngestorConsole/MainOrchestration.cs
@@ -43,7 +43,7 @@ namespace IngestorConsole
 
             Trace.WriteLine("Template:");
             Trace.WriteLine(template);
-            Trace.WriteLine();
+            Trace.WriteLine("");
 
             return new MainOrchestration(
                 generator,
