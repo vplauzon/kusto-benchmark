db2ea269377516b90a62b8022401f7f6f45c5a8a
vplauzon <vincent_lauzon@yahoo.com.au>
2025-01-27 23:22:54 -0500

    .

---
+++ code/EventHubConsole/CommandLineOptions.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHubConsole
+{
+    public class CommandLineOptions
+    {
+        [Option('l', "source-level", Required = false, HelpText = "Set source level")]
+        public string SourceLevel { get; set; } = "warning";
+
+        [Option(
+            't',
+            "template-text",
+            Required = true,
+            HelpText = "Set the template text")]
+        public string TemplateText { get; set; } = string.Empty;
+
+        [Option(
+            'r',
+            "rate",
+            Required = false,
+            HelpText = "Set the rate (in MBs/minute)")]
+        public long Rate { get; set; } = 10;
+
+        [Option('a', "auth", Required = false, HelpText = "Set authentication method:  'AzCli' or 'System'")]
+        public string Authentication { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $@"
+Template Text:  {TemplateText}
+Rate (MBs/minute):  {Rate}
+Authentication:  {Authentication}
+Source level:  {SourceLevel}";
+        }
+    }
+}
---
+++ code/EventHubConsole/EventHubOrchestration.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using BenchmarkLib;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHubConsole
+{
+    internal class EventHubOrchestration : IAsyncDisposable
+    {
+        private static readonly TimeSpan PERIOD = TimeSpan.FromSeconds(10);
+
+        private readonly ExpressionGenerator _generator;
+        private readonly ConcurrentQueue<Task> _queryTaskQueue = new();
+
+        #region Constructors
+        private EventHubOrchestration(
+            ExpressionGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public static async Task<EventHubOrchestration> CreateAsync(
+            CommandLineOptions options,
+            CancellationToken ct)
+        {
+            var credentials = CreateCredentials(options.Authentication);
+            var template = options.TemplateText;
+            var generator = await ExpressionGenerator.CreateAsync(template, kustoEngineClient, ct);
+
+            return new EventHubOrchestration(generator);
+        }
+
+        private static TokenCredential CreateCredentials(string authentication)
+        {
+            if (string.IsNullOrWhiteSpace(authentication)
+                || string.Compare(authentication.Trim(), "azcli", true) == 0)
+            {
+                return new DefaultAzureCredential();
+            }
+            else
+            {
+                return new ManagedIdentityCredential();
+            }
+        }
+        #endregion
+
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task ProcessAsync(CancellationToken ct)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
