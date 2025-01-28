9d3e4daa6e2f0a73a5ae3428e51b156752449ff2
vplauzon <vincent_lauzon@yahoo.com.au>
2025-01-28 16:27:56 -0500

    .

--- code/EventHubConsole/EventHubOrchestration.cs
+++ code/EventHubConsole/EventHubOrchestration.cs
@@ -3,6 +3,7 @@ using Azure.Messaging.EventHubs.Producer;
 using BenchmarkLib;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO.Compression;
 using System.Text;
 
@@ -62,21 +63,30 @@ namespace EventHubConsole
 
         public async Task ProcessAsync(CancellationToken ct)
         {
+            var metricWriter = new IngestionMetricWriter();
+
             using (var stream = new MemoryStream())
             {
                 while (!ct.IsCancellationRequested)
                 {
-                    var volume = await SendBatchAsync(stream, ct);
+                    await SendBatchAsync(stream, metricWriter, ct);
                 }
             }
         }
 
-        private async Task<long> SendBatchAsync(MemoryStream compressedStream, CancellationToken ct)
+        private async Task SendBatchAsync(
+            MemoryStream compressedStream,
+            IngestionMetricWriter metricWriter,
+            CancellationToken ct)
         {
-            long volume = 0;
-
             using (EventDataBatch eventBatch = await _eventHubProducerClient.CreateBatchAsync())
             {
+                long uncompressedVolume = 0;
+                long compressedVolume = 0;
+                long rowCount = 0;
+                var stopwatch = new Stopwatch();
+
+                stopwatch.Start();
                 for (var i = 0; i != _batchSize; ++i)
                 {
                     compressedStream.SetLength(0);
@@ -84,21 +94,33 @@ namespace EventHubConsole
                         new GZipStream(compressedStream, CompressionLevel.Fastest, true))
                     using (var writer = new StreamWriter(compressingStream))
                     {
+                        long payloadUncompressedVolume = 0;
+                        long payloadRowCount = 0;
+
                         for (var j = 0; j != _recordsPerPayload; ++j)
                         {
-                            volume += _generator.GenerateExpression(writer);
+                            payloadUncompressedVolume += _generator.GenerateExpression(writer);
                             writer.WriteLine();
-                            ++volume;
+                            ++payloadRowCount;
                         }
                         writer.Flush();
-                        eventBatch.TryAdd(new EventData(compressedStream.ToArray()));
+                        if (eventBatch.TryAdd(new EventData(compressedStream.ToArray())))
+                        {
+                            uncompressedVolume += payloadUncompressedVolume;
+                            rowCount += payloadRowCount;
+                            compressedVolume += compressedStream.Length;
+                        }
                     }
                 }
 
                 // Send the batch of events
                 await _eventHubProducerClient.SendAsync(eventBatch);
 
-                return volume;
+                metricWriter.Write(
+                    stopwatch.Elapsed,
+                    uncompressedVolume,
+                    compressedVolume,
+                    rowCount);
             }
         }
     }
