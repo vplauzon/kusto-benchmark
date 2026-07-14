42b0b60090e5a9e248c2f8e62768a606324cd462
vplauzon <vincent_lauzon@yahoo.com.au>
2026-07-14 17:01:52 -0400

    .

---
+++ code/EventHubExperimentConsole/CommandLineOptions.cs
@@ -0,0 +1,74 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHubExperimentConsole
+{
+    public class CommandLineOptions
+    {
+        [Option('l', "source-level", Required = false, HelpText = "Set source level")]
+        public string SourceLevel { get; set; } = "warning";
+
+        [Option('a', "auth", Required = false, HelpText = "Set authentication method:  'AzCli' or 'System'")]
+        public string Authentication { get; set; } = string.Empty;
+
+        [Option('f', "fqdn", Required = false, HelpText = "Set the fully qualified domain name (FQDN)")]
+        public string Fqdn { get; set; } = string.Empty;
+
+        [Option('e', "event-hub", Required = false, HelpText = "Set the event hub")]
+        public string EventHub { get; set; } = string.Empty;
+
+        [Option("event-hub-connection-string", Required = false, HelpText = "Set the event hub connection string")]
+        public string EventHubConnectionString { get; set; } = string.Empty;
+
+        [Option(
+            'd',
+            "db-uri",
+            Required = true,
+            HelpText = "Set the db URI, e.g. https://mycluster.westus.kusto.windows.net/mydb/")]
+        public string DbUri { get; set; } = string.Empty;
+
+        [Option(
+            't',
+            "template-name",
+            Required = true,
+            HelpText = "Set the template name")]
+        public string TemplateName { get; set; } = string.Empty;
+
+        [Option(
+            'r',
+            "records-per-payload",
+            Required = false,
+            HelpText = "Set the records per payload")]
+        public int RecordsPerPayload { get; set; } = 5;
+
+        [Option(
+            "throughput-target",
+            Required = false,
+            HelpText = "Set the throughput target, in MBs/minute")]
+        public int TargetThroughput { get; set; } = 100;
+
+        [Option(
+            'c',
+            "compression",
+            Required = false,
+            HelpText = "Set the output compression (true / false)")]
+        public bool? IsOutputCompressed { get; set; } = true;
+
+        public override string ToString()
+        {
+            return $@"
+Fqdn:  {Fqdn}
+EventHub:  {EventHub}
+Event Hub connection string:  {EventHubConnectionString}
+DbUri:  {DbUri}
+Template Name:  {TemplateName}
+RecordsPerPayload:  {RecordsPerPayload}
+TargetThroughput (in MBs/minute):  {TargetThroughput}
+IsOutputCompressed:  {IsOutputCompressed}
+Authentication:  {Authentication}
+Source level:  {SourceLevel}";
+        }
+    }
+}
---
+++ code/EventHubExperimentConsole/Program.cs
@@ -0,0 +1,95 @@
+﻿using Azure.Core.Diagnostics;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+namespace EventHubExperimentConsole
+{
+    internal class Program
+    {
+        public static string AssemblyVersion
+        {
+            get
+            {
+                var version = typeof(Program).Assembly.GetName().Version;
+                var versionText = version == null
+                    ? "<VERSION MISSING>"
+                    : version.ToString();
+
+                return versionText;
+            }
+        }
+
+        internal static async Task<int> Main(string[] args)
+        {
+            using var listener = CreateAzureEventSourceListener();
+
+            Console.WriteLine();
+            Console.WriteLine($"Kusto Event Hub Experiment Console {AssemblyVersion}");
+            Console.WriteLine();
+            Console.WriteLine($"Command line:  {string.Join(" ", args)}");
+            Console.Out.Flush();
+
+            try
+            {
+                return await ProgramHelper.RunAsync<CommandLineOptions>(args, RunOptionsAsync);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception:  {ex.Message}");
+
+                return -1;
+            }
+            finally
+            {
+                Console.WriteLine("Exiting...");
+            }
+        }
+
+        private static AzureEventSourceListener CreateAzureEventSourceListener()
+        {
+            var listener = new AzureEventSourceListener(
+                (eventArgs, message) =>
+                {
+                    Console.WriteLine($"Warning: {message}");
+                },
+                // Only log warnings and above
+                EventLevel.Warning);
+
+            return listener;
+        }
+
+        private static async Task RunOptionsAsync(CommandLineOptions options)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var taskCompletionSource = new TaskCompletionSource();
+
+            ProgramHelper.EnsureTraceLevel(options.SourceLevel);
+            AppDomain.CurrentDomain.ProcessExit += (e, s) =>
+            {
+                Trace.TraceInformation("Exiting process...");
+                cancellationTokenSource.Cancel();
+                taskCompletionSource.Task.Wait();
+            };
+            try
+            {
+                Trace.WriteLine("");
+                Trace.WriteLine("Parameterization:");
+                Trace.WriteLine("");
+                Trace.WriteLine(options.ToString());
+                Trace.WriteLine("");
+                await using (var orchestration = await EventHubOrchestration.CreateAsync(
+                    options,
+                    cancellationTokenSource.Token))
+                {
+                    Trace.WriteLine("Processing...");
+                    Trace.WriteLine("");
+                    await orchestration.ProcessAsync(cancellationTokenSource.Token);
+                }
+            }
+            finally
+            {
+                taskCompletionSource.SetResult();
+            }
+        }
+    }
+}
