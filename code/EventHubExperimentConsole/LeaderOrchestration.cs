74ada361350908d85b42cc520899169fa723f795
vplauzon <vincent_lauzon@yahoo.com.au>
2026-07-27 21:28:04 -0400

    .

--- code/EventHubExperimentConsole/LeaderOrchestration.cs
+++ code/EventHubExperimentConsole/LeaderOrchestration.cs
@@ -5,6 +5,8 @@ namespace EventHubExperimentConsole
 {
     internal class LeaderOrchestration
     {
+        private readonly static TimeSpan BEFORE_EXPERIMENT_DURATION = TimeSpan.FromSeconds(30);
+
         private readonly string _experimentName;
         private readonly ExperimentConfig _config;
         private readonly LogBlobClient<LogItem> _logBlobClient;
@@ -21,9 +23,13 @@ namespace EventHubExperimentConsole
 
         public async Task ProcessAsync(CancellationToken ct)
         {
-            var startTime = DateTime.Now.AddSeconds(30);
+            var allItems = await _logBlobClient.LoadAllAsync(ct);
+            var subExperimentItems = allItems.Result
+                .Where(i => i.SubExperimentItem != null)
+                .Select(i => i.SubExperimentItem);
+            var startTime = DateTime.Now.Add(BEFORE_EXPERIMENT_DURATION);
             var endTime = startTime.Add(_config.SubExperimentDuration);
-            var subExperimentItems = _config.SubExperiments
+            var newItems = _config.SubExperiments
                 .Select(s => LogItem.Create(new SubExperimentItem(
                     s.SubExperimentName,
                     1,
@@ -31,7 +37,7 @@ namespace EventHubExperimentConsole
                     startTime,
                     endTime)));
 
-            await _logBlobClient.AppendAsync(subExperimentItems, null, ct);
+            await _logBlobClient.AppendAsync(newItems, null, ct);
         }
     }
 }
